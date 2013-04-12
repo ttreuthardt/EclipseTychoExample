4e2cc7be946532527533c938fba7b7c17ad6d7ca
Thomas Treuthardt <thomas.treuthardt@gmail.com>
2013-04-12 21:13:47 +0200

    code coverage with jacoco sonar new common test plugin

--- li.obiwan.equinox.test.common/src/li/obiwan/equinox/test/common/Utility.java
+++ li.obiwan.equinox.test.common/src/li/obiwan/equinox/test/common/Utility.java
@@ -4,12 +4,19 @@ import org.apache.commons.io.FileSystemUtils;
 
 public class Utility {
 
+	private Utility() {
+	}
+
 	public static String getMyName() {
 		try {
-			FileSystemUtils.freeSpaceKb();			
-		} catch(Exception e) {
-			
+			FileSystemUtils.freeSpaceKb();
+		} catch (Exception e) {
+
 		}
+		return "Thomas";
+	}
+
+	public static String getName() {
 		return "Thomas";
 	}
 
